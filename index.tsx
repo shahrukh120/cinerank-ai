f93d8da063d3a9f04ba43cfe23ddb09ec24f5706
shahrukh120 <srkplayer47@gmail.com>
2025-12-31 17:40:41 +0530

    Added Vercel Analytics

--- index.tsx
+++ index.tsx
@@ -4,12 +4,13 @@ import App from './App.tsx'
 import './index.css'
 // 1. Import BrowserRouter
 import { BrowserRouter } from 'react-router-dom'
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* 2. Wrap App */}
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )
